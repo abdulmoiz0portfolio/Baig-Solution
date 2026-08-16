379bb359ec1d05d346b6aa01a49365a0392fed05
abdulmoiz0portfolio <bobrober2323@gmail.com>
2026-08-16 23:51:12 +0500

    chore: cache bust css to ensure orange scrollbars and shadows disappear for users

--- header.php
+++ header.php
@@ -216,7 +216,7 @@ $og_image = "{$protocol}://{$host}/assets/img/services/ai_automations.jpg";
     <link href="https://cdn.jsdelivr.net/npm/@n8n/chat/dist/style.css" rel="stylesheet" />
     
     <!-- Custom Main CSS -->
-    <link rel="stylesheet" href="assets/css/main.css?v=1786922891.47791">
+    <link rel="stylesheet" href="assets/css/main.css?v=1786924272.64168">
 </head>
 <body>
 
