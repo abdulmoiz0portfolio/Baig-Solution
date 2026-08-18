2517620079be124ee88e5c48ec113645ba97e624
Moiz Baig <moizbaig@example.com>
2026-08-19 02:18:29 +0500

    Clean up temporary python scripts

--- check_pixels.py
+++
@@ -1,6 +0,0 @@
-﻿from PIL import Image
-
-img_path = r'C:\Users\Moiz Baig\.gemini\antigravity\brain\26596035-7da0-4b63-9054-92fccbae5680\.user_uploaded\media_1787087085198.png'
-img = Image.open(img_path).convert('RGB')
-print('Top left pixel:', img.getpixel((0,0)))
-print('Center pixel:', img.getpixel((img.width//2, img.height//2)))
